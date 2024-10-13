4362e4ce9aff7403567b00104b27b9f01253ce85
Vitor Ramos <vitor_rolim11@hotmail.com.br>
2024-10-12 23:52:42 -0300

    Programa que cria o arquivo json e desserializa a classe email

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ArquivoJson.Modelos;
+using System.Text.Json;
+
+EmailJson vitoremail = new("Vitor", 25, "vitor.email@gmail.com");
+vitoremail.ConverterJson(vitoremail);
+
+using(HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("C:\\Users\\vitor\\OneDrive\\Área de Trabalho\\Área de trabalho\\.CursoC#-alura\\Projetos\\API\\ArquivoJson\\bin\\Debug\\net8.0\\email-Vitor-json");
+        var emailCadastrado = JsonSerializer.Deserialize<EmailJson>(resposta);
+        vitoremail.ConverterJson(emailCadastrado);
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine($"Erro: {ex.Message}");
+    }
+}
